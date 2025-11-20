f6ea67ef00b229e93067d78157a2b5e5b85ac3b7
killerwhalee <killerwhalee@kaist.ac.kr>
2025-11-20 19:27:47 +0000

    Added pool keywords

--- zindo/utils.py
+++ zindo/utils.py
@@ -21,7 +21,9 @@ def get_subject(title):
     # keyword pool
     pool = {
         "국어": ["국어", "독해", "문법", "읽기", "쓰기", "한글"],
-        "수학": ["수학", "연산", "계산"],
+        "수학": ["수학", "연산", "계산", "셈"],
+        "과학": ["과학",],
+        "영어": ["영어", "English", "english"]
     }
 
     # Run keyword search for each subject.
